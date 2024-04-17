7e4c241d99f6d133ceb1a1db744aa55e2ca79db4
Alpay <alpayc3@gmail.com>
2024-04-17 21:00:42 +0300

    bootstrap theme

--- client/crud/src/components/Header.jsx
+++ client/crud/src/components/Header.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav ">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Anasayfa</Nav.Link>
+            <NavDropdown title="Hakkımızda" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">ACM ?</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Team</NavDropdown.Item>
+              <NavDropdown.Divider />
+            </NavDropdown>
+            <Nav.Link href="#duyurular">Duyurular</Nav.Link>
+            <Nav.Link href="#blog">Blog</Nav.Link>
+            <Nav.Link href="#iletişim">İletişim</Nav.Link>
+            <Button>Sign İn</Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
